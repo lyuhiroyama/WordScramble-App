bc17051aea6bacf8e596cae19149ead54e2ce578
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-29 17:01:17 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Lyu Hiroyama on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
